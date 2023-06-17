53f6f6ba2672ee72714abec76372ebfb352732a7
Andrés Martin Mena <128084150+AndresMartinM@users.noreply.github.com>
2023-06-17 02:48:15 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ async function tarjetasm() {
     const data = await consulta.json();
     const donde = document.querySelectorAll(".tarjeta-sm");
     donde.forEach(function (t, i) {
-        donde[i].innerHTML += '<a href="" ><div class="tarjeta" style="background : url('+ data[i+3].image[0] +'); background-repeat: no-repeat; background-size: 100% auto;"><div class="sombra"></div><h3>' + data[i+3].title + '<p>' + data[i+3].text + '</p></h3><h4>'+data[i+3].year +'</h4></div></a>';
+        donde[i].innerHTML += '<a href="articulo.html?nro='+(i+3)+'" ><div class="tarjeta" style="background : url('+ data[i+3].image[0] +'); background-repeat: no-repeat; background-size: 100% auto;"><div class="sombra"></div><h3>' + data[i+3].title + '<p>' + data[i+3].text + '</p></h3><h4>'+data[i+3].year +'</h4></div></a>';
     });
 }
 async function tarjetamd() {
@@ -12,7 +12,7 @@ async function tarjetamd() {
     const data = await consulta.json();
     const donde = document.querySelectorAll(".tarjeta-md");
     donde.forEach(function (t, i) {
-        donde[i].innerHTML += '<a href="" ><div class="tarjeta" style="background : url('+ data[i+1].image[0] +'); background-repeat: no-repeat; background-size: 100% auto;"><div class="sombra"></div><h3>' + data[i+1].title + '<p>' + data[i+1].text + '</p></h3><h4>'+data[i+1].year +'</h4></div></a>';
+        donde[i].innerHTML += '<a href="articulo.html?nro='+(i+1)+'" ><div class="tarjeta" style="background : url('+ data[i+1].image[0] +'); background-repeat: no-repeat; background-size: 100% auto;"><div class="sombra"></div><h3>' + data[i+1].title + '<p>' + data[i+1].text + '</p></h3><h4>'+data[i+1].year +'</h4></div></a>';
     });
 }
 async function tarjetalg() {
@@ -20,7 +20,7 @@ async function tarjetalg() {
     const data = await consulta.json();
     const donde = document.querySelectorAll(".tarjeta-lg");
     donde.forEach(function (t, i) {
-        donde[i].innerHTML += '<a href="" ><div class="tarjeta" style="background : url('+ data[i].image[0] +'); background-repeat: no-repeat; background-size: 100% auto;"><div class="sombra"></div><h3>' + data[i].title + '<p>' + data[i].text + '</p></h3><h4>'+data[i].year +'</h4></div></a>';
+        donde[i].innerHTML += '<a href="articulo.html?nro='+i+'" ><div class="tarjeta" style="background : url('+ data[i].image[0] +'); background-repeat: no-repeat; background-size: 100% auto;"><div class="sombra"></div><h3>' + data[i].title + '<p>' + data[i].text + '</p></h3><h4>'+data[i].year +'</h4></div></a>';
     });
 }
               
