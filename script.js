88c7b5b0b6eb9d6876fce4d79a2c2039b96cc6c6
Andrés Martin Mena <128084150+AndresMartinM@users.noreply.github.com>
2023-06-24 08:07:03 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+async function articulo() {
+	var n = new URLSearchParams(window.location.search).get("nro");
+	n = Number(n);
+	const consulta = await fetch("https://raw.githubusercontent.com/AndresMartinM/portafolioAnim2023/main/works.json");
+    const data = await consulta.json();
+    document.querySelector("#texto").innerHTML = '<div><small class="float-end">' + data[n].year + '</small><h1>' + data[n].title + '</h1><h2>' + data[n].text[0] + '</h2><div id="cuerpo-texto"></div></div>';
+	data[n].text.forEach((x,i) => {
+		if(i>0)document.querySelector("#cuerpo-texto").innerHTML += '<p>'+ x +'</p>';
+	});
+    data[n].image.forEach((x) => {
+		document.querySelector("#fotos").innerHTML += '<img src="' + x + '" width="600px" class="w-100" alt="imagen del proyecto">'
+    });
+}
+
 async function tarjetasm() {
     const consulta = await fetch("https://raw.githubusercontent.com/AndresMartinM/portafolioAnim2023/main/works.json");
     const data = await consulta.json();
@@ -23,6 +37,7 @@ async function tarjetalg() {
     });
 }
               
+articulo().catch((error) => console.error(error));
 tarjetasm().catch((error) => console.error(error));
 tarjetamd().catch((error) => console.error(error));
 tarjetalg().catch((error) => console.error(error));
